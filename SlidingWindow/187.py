0cd2ba0e62d4261fc13b083bbded312bb765b42d
oliveiramnando <oliveiramnando@gmail.com>
2025-04-02 12:22:45 -0400

    Repeated DNA Sequences

--- SlidingWindow/187.py
+++ SlidingWindow/187.py
@@ -22,3 +22,16 @@ class Solution:
                 res.append(string)
 
         return res
+
+# ---------- Optimization ---------- #
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        ans, seen = set(), set()
+        for i in range(len(s)-9):
+            dna = s[i:i+10]
+            if dna in seen:
+                ans.add(dna)
+            else:
+                seen.add(dna)
+        return list(ans)
+

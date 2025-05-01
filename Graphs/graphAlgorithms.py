b74212fd48eb0f8824dc6c7451088b3184601e67
oliveiramnando <oliveiramnando@gmail.com>
2025-05-01 11:58:01 -0400

    dijkstrasV2

--- Graphs/graphAlgorithms.py
+++ Graphs/graphAlgorithms.py
@@ -45,6 +45,28 @@ class Algorithms:
                 distances[node] = -1
 
         return distances
+
+    def dijkstrasVTwo(graph, n: int, edges: List[List[int]], src:int)->Dict[int, int]:
+        adj = {i: [] for i in range(1, n + 1)}
+        for s, d, w in times:
+            adj[s].append((d, w))
+
+        shortest = {}
+        minHeap = [(0, k)] 
+
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            if n1 in shortest:
+                continue
+            shortest[n1] = w1
+            for n2, w2 in adj[n1]:
+                if n2 not in shortest:
+                    heapq.heappush(minHeap, (w1 + w2, n2))
+
+        if len(shortest) < n:
+            return -1
+
+        return shortest
     
     def bellman_ford(V, edges, src):
         # Initially distance from source to all other vertices 

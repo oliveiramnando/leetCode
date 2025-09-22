603b7e1908fa9f94e97ed3595523f99718766eeb
oliveiramnando <oliveiramnando@gmail.com>
2025-09-21 21:38:55 -0400

    Encode and Decode Strings

--- ArraysAndHashing/271.py
+++ ArraysAndHashing/271.py
@@ -1,31 +1,22 @@
 class Solution:
     def encode(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        sizes, res = [], ""
+        res = ""
         for s in strs:
-            sizes.append(len(s))
-        for sz in sizes:
-            res += str(sz)
-            res += ','
-        res += '#'
-        for s in strs:
-            res += s
+            res += str(len(s)) + "#" + s
         return res
 
     def decode(self, s: str) -> List[str]:
-        if not s:
-            return []
-        sizes, res, i = [], [], 0
-        while s[i] != '#':
-            cur = ""
-            while s[i] != ',':
-                cur += s[i]
-                i += 1
-            sizes.append(int(cur))
-            i += 1
-        i += 1
-        for sz in sizes:
-            res.append(s[i:i + sz])
-            i += sz
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+
         return res
+

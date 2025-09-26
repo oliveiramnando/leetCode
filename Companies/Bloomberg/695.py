873d3f43b51e79cd3de885d054b1b8a0bffed618
oliveiramnando <oliveiramnando@gmail.com>
2025-09-26 14:31:34 -0400

    Max Area of Island

--- Companies/Bloomberg/695.py
+++ Companies/Bloomberg/695.py
@@ -4,17 +4,17 @@ class Solution:
         visited = set()
         max_island = 0
         
-        def bfs(r, c):
+        def dfs(r, c):
             if r not in range(rows) or c not in range(cols) or (r,c) in visited or grid[r][c] == 0:
                 return 0
             
             visited.add((r,c))
 
-            return 1 + bfs(r + 1, c) + bfs(r - 1, c) + bfs(r, c + 1) + bfs(r, c - 1)
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1 and (r,c) not in visited:
-                    max_island = max(max_island, bfs(r,c))
+                    max_island = max(max_island, dfs(r,c))
         
         return max_island

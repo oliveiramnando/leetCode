5e93f24c2002dcb5143c35fc51a2a06c7ffdfca4
oliveiramnando <oliveiramnando@gmail.com>
2026-01-15 16:14:41 -0500

    Linked List Cycle

--- LinkedLists/141.py
+++ LinkedLists/141.py
@@ -15,4 +15,14 @@ class Solution:
             curr = curr.next
         return False
 
-        
+# two pointers solution: 0(n) time 0(1) space
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False        

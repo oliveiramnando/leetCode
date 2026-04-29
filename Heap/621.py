941f2b0c6642434e98b0768acefbddbd945d1fe4
oliveiramnando <oliveiramnando@gmail.com>
2026-04-29 17:30:56 -0400

    Task Scheduler

---
+++ Heap/621.py
@@ -0,0 +1,23 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque()     # stores [remaining_count_after_running, next_available_time]
+
+        while maxHeap or q:
+            time += 1
+
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                cnt = 1 + heapq.heappop(maxHeap) # add 1 bc values of maxheap are negative
+                if cnt:
+                    q.append([cnt, time + n])
+            
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        
+        return time

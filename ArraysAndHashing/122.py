3b01030c1fd3ddeb5350f05f515eebb95ef35ee0
oliveiramnando <oliveiramnando@gmail.com>
2026-07-21 12:52:36 -0400

    Best Time to Buy and Sell Stock II

---
+++ ArraysAndHashing/122.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        max_profit = 0
+        min_buy = prices[0]
+        
+        for i in range(len(prices)):
+            min_buy = min(prices[i], min_buy)
+            profit = prices[i] - min_buy
+
+            if profit + max_profit > max_profit:
+                max_profit += profit
+                min_buy = prices[i]
+          
+            
+        return max_profit

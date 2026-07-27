be180ee589b12eb6a8912c84a370c495c52e7b7f
oliveiramnando <oliveiramnando@gmail.com>
2026-07-26 22:40:42 -0400

    Maximum Product of Two Elements in an Array

--- dailyChallenge/1464.py
+++ dailyChallenge/1464.py
@@ -1,10 +1,22 @@
+#class Solution:
+#    def maxProduct(self, nums: List[int]) -> int:
+#        maxProd = float('-inf')
+#
+#        for i in range(len(nums)):
+#            for j in range(i+1, len(nums)):
+#                prod = (nums[i]-1) * (nums[j]-1)
+#                maxProd = max(maxProd, prod)
+#        
+#        return maxProd
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        maxProd = float('-inf')
+        x, y = 0, 0 # x for the abs max, and y for the second largest value
 
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                prod = (nums[i]-1) * (nums[j]-1)
-                maxProd = max(maxProd, prod)
-        
-        return maxProd
+        for n in nums:
+            if n > x:         # if larger than current largest val
+                y, x = x, n   # move current largest val to second spot, update largest val
+            else:
+                y = max(y, n)
+
+        return (x-1) * (y-1)

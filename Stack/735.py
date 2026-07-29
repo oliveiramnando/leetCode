098d220922230e6aba297772cecf0daf61635ff8
oliveiramnando <oliveiramnando@gmail.com>
2026-07-29 14:35:30 -0400

    Asteroid Collision

--- Stack/735.py
+++ Stack/735.py
@@ -1,5 +1,28 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        def collides(x, y):
+            if ((x < 0) and (y > 0)):
+                return True
+            return False
+        
+        res = []
+
+        for a in asteroids:
+            while res and collides(a, res[-1]) and abs(a) > abs(res[-1]):
+                res.pop()
+
+            if res and collides(a, res[-1]) and abs(a) == abs(res[-1]):
+                res.pop()
+                continue
+
+            if not res or not collides(a, res[-1]):
+                res.append(a)
+     
+        return res
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
         for a in asteroids:
             while stack and a < 0 and stack[-1] > 0:

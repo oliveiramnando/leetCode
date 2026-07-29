a1053291bfe2c41d29a33b6c73e48f0a4f4ad96d
oliveiramnando <oliveiramnando@gmail.com>
2026-07-28 20:49:22 -0400

    Binary Tree Inorder Traversal

--- Trees/94.py
+++ Trees/94.py
@@ -1,16 +1,13 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def inorderTraversal(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[int]
-        """
-        if root == None:
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
             return []
-
+        
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+

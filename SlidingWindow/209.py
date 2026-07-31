87eff25be77ca586dfd2a08d3bc67963225b7e99
oliveiramnando <oliveiramnando@gmail.com>
2026-07-31 14:10:21 -0400

    Minimum Size Subarray Sum

--- SlidingWindow/209.py
+++ SlidingWindow/209.py
@@ -1,48 +1,18 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        min_len = float('inf')
-        sumOfNums = 0
+        res = float('inf')
         left = 0
 
+        currSum = 0
         for right in range(len(nums)):
-            sumOfNums += nums[right]
-
-            #check if its equal to target, if it is update length
-            if sumOfNums >= target:
-                min_len = min(min_len, right - left + 1)
-
-            while sumOfNums > target: # constrict window
-                sumOfNums -= nums[left]
-                left += 1
-                if sumOfNums < target:
-                    left -= 1
-                    sumOfNums += nums[left]
-                    min_len = min(min_len, right - left + 1)
-                    break
+            currSum += nums[right]
             
-            #check if its equal to target, if it is update length
-            if sumOfNums >= target:
-                min_len = min(min_len, right - left + 1)
+            while currSum >= target:
+                res = min(res, right-left+1)
 
-        if sumOfNums < target:
-            return 0
-
-        return min_len
-
-# ---------- Optimization ---------- #
-class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        min_len = float("inf")
-        left = 0
-        cur_sum = 0
-
-        for right in range(len(nums)):
-            cur_sum += nums[right]
-
-            while cur_sum >= target:
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                cur_sum -= nums[left]
+                currSum -= nums[left]
                 left += 1
+            
         
-        return min_len if min_len != float("inf") else 0
+        return res if res!=float('inf') else 0
+        

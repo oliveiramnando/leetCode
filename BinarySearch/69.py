82aff4e2cb4af81bc7ce05b4c07536d4d067d31f
oliveiramnando <oliveiramnando@gmail.com>
2026-08-01 15:11:47 -0400

    Sqrt(x)

--- BinarySearch/69.py
+++ BinarySearch/69.py
@@ -1,5 +1,23 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        res = 0
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if m * m > x:
+                r = m - 1
+            elif m * m < x:
+                l = m + 1
+                res = m
+            else:
+                return m
+
+        return res
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
         if x==2 or x==3:
             return 1
         

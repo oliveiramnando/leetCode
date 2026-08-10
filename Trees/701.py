0df05788dd22a45d91d552f0da28f6d4198a94cb
oliveiramnando <oliveiramnando@gmail.com>
2026-08-10 11:47:50 -0400

    Insert into a Binary Search Tree

--- Trees/701.py
+++ Trees/701.py
@@ -6,39 +6,13 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        node = TreeNode(val)
         if not root:
-            return node
-        self.helper(root, node)
+            return TreeNode(val)
+
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        
         return root
     
-    def helper(self, root, node):
-        if not root:
-            root = node
-            return root
-        
-        if root.left is None and root.right is None:
-            if node.val < root.val:
-                root.left = self.helper(root.left, node)
-            else:
-                root.right = self.helper(root.right, node)
-            return
-
-        elif root.left is None and node.val < root.val:
-            root.left = self.helper(root.left, node)
-            return
-        elif root.right is None and node.val > root.val:
-            root.right = self.helper(root.right, node)
-            return
-        
-        else:
-            if node.val > root.val:
-                self.helper(root.right, node)
-            else:
-                self.helper(root.left, node)
-    
-
-
-        
-        
-

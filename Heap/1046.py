47123082ceb61f5a60dc9da5159f711be4513475
oliveiramnando <oliveiramnando@gmail.com>
2026-08-31 16:08:09 -0400

    Last Stone Weight

--- Heap/1046.py
+++ Heap/1046.py
@@ -1,5 +1,20 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = [-n for n in stones] 
+        heapq.heapify(heap)
+
+        while len(heap) > 1:
+            y = -(heapq.heappop(heap))
+            x = -(heapq.heappop(heap))
+
+            smash = y - x
+            if smash != 0:
+                heapq.heappush(heap, -(smash))
+
+        return -(heap[0]) if heap else 0
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
         maxHeap = [-s for s in stones]
         heapq.heapify(maxHeap)
 
